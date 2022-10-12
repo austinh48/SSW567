a788483f64fe5ee8a5ae80b850554ca067713457
austinhodges48 <austin.hodges48@yahoo.com>
2022-10-12 15:37:30 -0400

    WORK

--- GitHubAPI-hw4/homework04a.py
+++ GitHubAPI-hw4/homework04a.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest import mock
+import homework04a
 import requests
 import json
 
@@ -7,7 +9,6 @@ def getRepos(id, repoName):
 
     gitRepo = requests.get('https://api.github.com/users/'+id+'/repos')
     
-
     repoInfo = json.loads(gitRepo.text)
 
     for x in repoInfo:
@@ -20,13 +21,27 @@ def getRepos(id, repoName):
 
     
 class testRepo(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(getRepos('austinh48','SSW555'),'Repo: SSW555 Number of commits: 5', 'Should be 5')
-    def test2(self):
-        self.assertEqual(getRepos('austinh48','SSW567'),'Repo: SSW555 Number of commits: 22', 'Should be 22, maybe 23 once this is submitted')
 
+    @mock.patch('homework04a.getRepos')
+    def mock_get_repos(mock_get_re):
+        # print(mock_get_re)
+        # print(homework04a.getRepos)
+        mock_get_re.return_value = ['Repo: SSW555 Number of commits: 5', 'Repo: SSW555 Number of commits: 22']
+        result = homework04a.getRepos()
+        return result
+
+    mock_get_repos()
+
+    def test1(self):
+        from homework04a import testRepo
+        self.assertEqual(testRepo.mock_get_repos()[0],'Repo: SSW555 Number of commits: 5', 'Should be 5')
+    def test2(self):
+        from homework04a import testRepo
+        self.assertEqual(testRepo.mock_get_repos()[1],'Repo: SSW555 Number of commits: 22', 'Should be 22, maybe 23 once this is submitted')
+ 
+    
 
 if __name__ == '__main__':
-    getRepos('austinh48','all')
+    # getRepos('austinh48','all')
 
     unittest.main(exit = True)
